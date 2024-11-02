0ef39bc88cfedd1a145ad366010eb647dfcc23c0
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-11-01 22:38:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ import numpy as np
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
-
+ax.set_ylabel('Puntuación (0-10)')
+ax.set_title('Comparación de Soporte de GPU por Sistema Operativo')
 st.pyplot(fig)
